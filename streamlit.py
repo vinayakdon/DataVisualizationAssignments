d7ee8dad75d4a5627eda662663c23dba78ac217c
Vinayak Donawad <78019838+vinayakdon@users.noreply.github.com>
2026-01-15 21:01:41 +0100

    Update page title in Streamlit app

--- streamlit.py
+++ streamlit.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 st.set_page_config(
-    page_title="Bike Sharing Dashboard (Cleaned Data)",
+    page_title="Bike Sharing Dashboard",
     layout="wide",
     initial_sidebar_state="expanded",
 )
